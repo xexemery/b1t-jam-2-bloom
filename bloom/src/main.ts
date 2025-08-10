0b2d78fbb59766a0b52ff37b3e17eab19a68c9b2
emery <215300895+xexemery@users.noreply.github.com>
2025-08-11 00:09:37 +0100

    Remove default sprite

--- bloom/src/main.ts
+++ bloom/src/main.ts
@@ -4,8 +4,3 @@ import "kaplay/global";
 kaplay();
 
 loadRoot("./"); // for itch.io publishing purposes
-loadSprite("bean", "sprites/bean.png");
-
-add([pos(120, 80), sprite("bean")]);
-
-onClick(() => addKaboom(mousePos()));
