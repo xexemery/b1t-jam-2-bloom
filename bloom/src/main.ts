2f7328e2a62dce2fde6e6ea046c0d134565d2af8
emery <215300895+xexemery@users.noreply.github.com>
2025-08-11 21:44:50 +0100

    Add kaplay options

--- bloom/src/main.ts
+++ bloom/src/main.ts
@@ -1,6 +1,12 @@
 import kaplay from "kaplay";
-import "kaplay/global";
 
-kaplay();
+const k = kaplay({
+  width: 320,
+  height: 240,
+  stretch: true,
+  letterbox: true,
+  crisp: true,
+  global: false,
+});
 
-loadRoot("./"); // for itch.io publishing purposes
+k.loadRoot("./"); // for itch.io publishing purposes
