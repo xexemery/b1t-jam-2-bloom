9892b2d9bb53bedb8fed9ba0ac7700839b8513b7
emery <215300895+xexemery@users.noreply.github.com>
2025-08-15 00:24:13 +0100

    Add background and main sprite

--- bloom/src/main.ts
+++ bloom/src/main.ts
@@ -6,7 +6,20 @@ const k = kaplay({
   stretch: true,
   letterbox: true,
   crisp: true,
+  background: "#4a3052",
   global: false,
 });
 
 k.loadRoot("./"); // for itch.io publishing purposes
+
+k.loadSprite("flower", "sprites/flower.png", {
+  sliceX: 3,
+  sliceY: 1,
+});
+
+const flower = k.add([
+  k.sprite("flower", {
+    frame: 0,
+  }),
+  k.pos(144, 104),
+]);
